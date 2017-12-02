9a14d0cf044112193600064bf074f29d17f962d1
Rosabelle Armstead <ra00528@surrey.ac.uk>
2017-12-02 09:50:37 +0000

    Added home controller testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,9 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Music Library'
+    assert_select 'h1', 'Music Library'
+    assert_template layout: 'application'
   end
 
 end
