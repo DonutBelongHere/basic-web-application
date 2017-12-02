7bdd5a99c015383799e6906bc1d98c6f2726f414
Rosabelle Armstead <ra00528@surrey.ac.uk>
2017-12-02 10:49:22 +0000

    Added contact action testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -8,5 +8,14 @@ class HomeControllerTest < ActionController::TestCase
     assert_select 'h1', 'Music Library'
     assert_template layout: 'application'
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select 'title', 'Music Library'
+    assert_select 'h3', 'Contact Us'
+    assert_select 'p', 'Please fill out the form to get in touch.'
+    assert_template layout: 'application'
+  end
 
 end
