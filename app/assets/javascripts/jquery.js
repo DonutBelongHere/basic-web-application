c6e1f4099386a791e18162e4d9ee874a683e66fb
Rosabelle Armstead <ra00528@surrey.ac.uk>
2017-12-08 14:04:16 +0000

    Added more styling and navigation to home page

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -3,6 +3,7 @@ $(document).on('turbolinks:load', function() {
    //accordion view on home page for ease of viewing many albums
    $('#menu').accordion({collapsible: true, active: 'none'}, "option", "autoHeight", false );
    //fade buttons when mouseover, restore when mouseleave
+   //separate functions for each button to prevent all buttons fading when only one mouseover
    $('div#button').mouseenter(function() {
        $('div#button').fadeTo('fast', 0.7);
    });
@@ -21,6 +22,13 @@ $(document).on('turbolinks:load', function() {
    $('div#backbutton').mouseleave(function() {
        $('div#backbutton').fadeTo('fast', 1);
    });
-   //dialog box for notices to make it easier for user to see them
+   $('div#newbutton').mouseenter(function() {
+       $('div#newbutton').fadeTo('fast', 0.7);
+   });
+   $('div#newbutton').mouseleave(function() {
+       $('div#newbutton').fadeTo('fast', 1);
+   });
+   //dialog boxes for notices and alerts to make it easier for user to see them
    $( "#notice" ).dialog();
+   $( "#alert" ).dialog();
 });
